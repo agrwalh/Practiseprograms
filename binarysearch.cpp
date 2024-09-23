2c1ff2a8c605aaa64ed9da4d04d9749f68a59184
Harsh Agarwal <130899605+agrwalh@users.noreply.github.com>
2024-09-23 19:59:53 +0530

    first commit

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,34 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int binarysearch(int a[],int size,int key){
-    int start=0,end=size-1;
-     int mid=start+(end-start)/2;
-    while(start<=end){
-        if(a[mid]==key)
+
+void linearsearch(int a[], int size, int key)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (a[i] == key)
+        {
+            cout << "The searched element is at:" << i << endl;
+            
+        }
+    }
+}
+int binarysearch(int a[], int size, int key)
+{
+    int start = 0, end = size - 1;
+    int mid = start + (end - start) / 2;
+    while (start <= end)
+    {
+        if (a[mid] == key)
         {
             return mid;
         }
-        else if(a[mid]<key)
+        else if (a[mid] < key)
         {
-            start=mid+1;
-
+            start = mid + 1;
         }
         else
         {
-            end=mid-1;
+            end = mid - 1;
         }
-        mid=start+(end-start)/2;
-
+        mid = start + (end - start) / 2;
     }
     return -1;
 }
-int main(){
-    int even[6]={2,3,4,5,6,7};
-    int odd[5]={8,9,10,11,45};
+int main()
+{
+    int even[6] = {2, 3, 4, 5, 6, 7};
+    int odd[5] = {8, 9, 10, 11, 45};
     // int index= binarysearch(even,6,6);
-    int index= binarysearch(odd,5,45);
+    int index = binarysearch(odd, 5, 45);
+    linearsearch(even, 6, 4);
 
-    cout<<"Index of 45 is : "<< index << endl;
+    cout << "Index of 45 is : " << index << endl;
 
     return 0;
 }
