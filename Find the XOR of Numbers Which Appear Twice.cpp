f4e04cdf5ffc5ce3bd8f494e7a53805dc225eef4
HA <haagarwal67@gmail.com>
2024-09-29 18:10:24 +0530

    first commit

---
+++ Find the XOR of Numbers Which Appear Twice.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int duplicateNumbersXOR(vector<int>& nums) {
+        
+        map<int ,int> mp;
+        vector<int> ans;
+        for(auto i: nums)
+        {
+        mp[i]++;
+        }  
+
+        for(auto i:mp){
+            if(i.second==2){
+                ans.push_back(i.first);
+            }
+        
+        }
+        int k=0;
+        if(ans.empty())
+        {
+            return 0;
+        }
+      for(auto i:ans){
+        k=k^i;
+      }
+      return k;
+
+    }
+};
